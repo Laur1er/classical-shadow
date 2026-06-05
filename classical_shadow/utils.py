abc909dbc8c3b542e5f2f5eac73a9bf5c2167f9d
perl2548 <perl2548@usherbrooke.ca>
2026-06-05 10:33:59 -0800

    Local Shadows in progress

---
+++ classical_shadow/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+
+
+def run_circuit_once(circuit: QuantumCircuit):
+    """
+    Simulate the circuit for one single shot using AerSimulator.
+
+    Params:
+        circuit (QuantumCircuit): The state to be measured
+    Returns:
+        String: bitstring measured (little-endian)
+    """
+    simulator = AerSimulator()
+    compiled = transpile(circuit, simulator)
+    job = simulator.run(compiled, shots=1)
+    counts = job.result().get_counts()
+
+    return list(counts.keys())[0]
